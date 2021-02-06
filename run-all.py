9a95bafce6e511529e486a64ad8641b72c158e59
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-02-06 22:53:05 +0100

    simplify audio handling: implicitely extract it from the first input (assumes audios from all contents are the same)

--- run-all.py
+++ run-all.py
@@ -7,13 +7,14 @@ import os.path
 import json
 import pysftp
 
-# output: SFTP credentials
+# Output: SFTP credentials
 host = "dashstorage.upload.akamai.com"
 username = "sshacs"
 cnopts = pysftp.CnOpts(knownhosts=host)
 cnopts.hostkeys = None    
 outputFolder = '/129021/dash/WAVE/vectors/'
 
+# TODO: link to params.csv - currently only references Switching Set X1
 resolutions = [
     [
         ['1920x1080', 4500, 30, "content_files/tos_L1_1920x1080@30_60.mp4" ],
@@ -32,7 +33,6 @@ with open('params.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     id = "wave_{0}_{1}"
-    has_audio = False # we only support one audio for now
     for row in csv_reader:
         line_count = line_count + 1
         if line_count == 1: 
@@ -43,43 +43,30 @@ with open('params.csv') as csv_file:
 
         for i in range(len(resolutions[0])):
             reps += [{"resolution": resolutions[0][i][0], "framerate": resolutions[0][i][2], "bitrate": resolutions[0][i][1], "input": resolutions[0][i][3]}]
-            if row[1] == "audio":
-                if has_audio == False:
-                    codec="aac"
-                    audio_rep_command = "id:{0},type:{1},codec:{2},bitrate:{3},input:{4}"\
-                        .format(i, row[1], codec, resolutions[0][i][1], resolutions[0][i][3])
-                    has_audio=True
-            else:
-                codec="h264"
-                cmaf_profile="avchdhf"
-                reps_command += "id:{0},type:{1},codec:{2},vse:{3},cmaf:{4},fps:{5},res:{6},bitrate:{7},input:{8},sei:{9},vui_timing:{10}"\
-                    .format(i, row[1], codec, row[4], cmaf_profile, resolutions[0][i][2], resolutions[0][i][0], resolutions[0][i][1], resolutions[0][i][3], row[2].capitalize(), row[3].capitalize())
+            codec="h264"
+            cmaf_profile="avchdhf"
+            reps_command += "id:{0},type:video,codec:{1},vse:{2},cmaf:{3},fps:{4},res:{5},bitrate:{6},input:{7},sei:{8},vui_timing:{9}"\
+                .format(i, codec, row[3], cmaf_profile, resolutions[0][i][2], resolutions[0][i][0], resolutions[0][i][1], resolutions[0][i][3], row[1].capitalize(), row[2].capitalize())
 
             reps_command += "\|"
 
-        if has_audio == False:
-            print("Audio is mandatory. Make sure the audio stream is the first listed stream in the input csv.")
-            exit(1)
+        #add audio
+        reps_command += "id:{0},type:audio,codec:aac,bitrate:{1},input:{2}"\
+            .format(len(resolutions[0])+1, resolutions[0][i][1], resolutions[0][i][3])
 
-        if reps_command == None:
-            print("Audio only streams are not processed (but the audio will be then attached to video streams) - skipping")
-        else:
-            database[key] = {
-                'representations': reps,
-                'segmentDuration': row[6], 
-                'fragmentType': row[8], 
-                'hasSEI': row[2].lower() == 'true', 
-                'hasVUITiming': row[3].lower()== 'true', 
-                'visualSampleEntry': row[4],
-                'mpdPath': 'avc_sets/{0}/stream.mpd'.format(row[0])
-            }
+        database[key] = {
+            'representations': reps,
+            'segmentDuration': row[5],
+            'fragmentType': row[7],
+            'hasSEI': row[1].lower() == 'true',
+            'hasVUITiming': row[2].lower() == 'true',
+            'visualSampleEntry': row[3],
+            'mpdPath': 'avc_sets/{0}/stream.mpd'.format(row[0])
+        }
 
-            # add audio
-            reps_command += audio_rep_command
-
-            command = "./encode_dash.py --path=/usr/bin/ffmpeg --out=stream.mpd --outdir=output/{0} --dash=sd:{1},ft:{2} {3}".format(row[0], row[6], row[8], reps_command)
-            print("Executing " + command)
-            result = subprocess.run(command, shell=True)
+        command = "./encode_dash.py --path=/usr/bin/ffmpeg --out=stream.mpd --outdir=output/{0} --dash=sd:{1},ft:{2} {3}".format(row[0], row[5], row[7], reps_command)
+        print("Executing " + command)
+        result = subprocess.run(command, shell=True)
 
 # Write the database to a file
 with open(filepath, 'w') as outfile:
