c4d5f270263f2e3ab9d354756074a01939c1d20d
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-04-28 12:05:15 +0200

    process both single track and X1 switching sets

--- run-all.py
+++ run-all.py
@@ -7,56 +7,63 @@ import os.path
 import json
 import pysftp
 
-# Output: SFTP credentials
+# Output parameters: SFTP credentials
 host = "dashstorage.upload.akamai.com"
 username = "sshacs"
 cnopts = pysftp.CnOpts(knownhosts=host)
 cnopts.hostkeys = None    
 outputFolder = '/129021/dash/WAVE/vectors/'
+database = { } # Romain: contains single track only
+wwwfilepath = './database.json'
 
-# TODO: link to params.csv - currently only references Switching Set X1
-resolutions = [
-    [
-        ['1920x1080', 6000, 60, "content_files/tos_L2_1920x1080@60_60.mp4" ],
-        ['1920x1080', 4500, 30, "content_files/tos_L1_1920x1080@30_60.mp4" ],
-        ['1280x720' , 3000, 60, "content_files/tos_J1_1280x720@60_60.mp4"  ],
-        ['1024x576' , 1500, 30, "content_files/tos_I2_1024x576@30_60.mp4"  ],
-        ['1024x576' , 1200, 30, "content_files/tos_I1_1024x576@30_60.mp4"  ],
-        ['768x432'  , 900 , 30, "content_files/tos_F1_768x432@30_60.mp4"   ],
-        ['512x288'  , 450 , 30, "content_files/tos_B1_512x288@30_60.mp4"   ],
-        ['480x270'  , 300 , 15, "content_files/tos_A1_480x270@15_60.mp4"   ]
-    ]
-]
-
-database = { }
-filepath = './database.json'
-
-# Open the input parameter params
-with open('params.csv') as csv_file:
+with open('switching_sets_single_track.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    id = "wave_{0}_{1}"
+
+    switching_set_X1_IDs = { 20, 1, 23, 24, 25, 28, 32, 34 }
+    switching_set_X1_command = ""
+
+    #TODO: fps multiples should be a parameter
+    content = "avc/15_30_60/{0}{1}"
+    fps_base = 60
+
     for row in csv_reader:
         line_count = line_count + 1
-        if line_count == 1: 
+        if line_count == 1:
             continue
+
+        key = content.format("t", row[0])
+        print("Processing single track Switching Set " + key)
+
         reps_command = "--reps="
-        key = id.format("avc_sets", row[0])
         reps = []
 
-        for i in range(len(resolutions[0])):
-            reps += [{"resolution": resolutions[0][i][0], "framerate": resolutions[0][i][2], "bitrate": resolutions[0][i][1], "input": resolutions[0][i][3]}]
-            codec="h264"
-            cmaf_profile="avchdhf"
-            reps_command += "id:{0},type:video,codec:{1},vse:{2},cmaf:{3},fps:{4},res:{5},bitrate:{6},input:{7},sei:{8},vui_timing:{9}"\
-                .format(i, codec, row[3], cmaf_profile, resolutions[0][i][2], resolutions[0][i][0], resolutions[0][i][1], resolutions[0][i][3], row[1].capitalize(), row[2].capitalize())
+        # 0: Stream ID, 1: mezzanine prefix, 2: pic timing, 3: VUI timing, 4: sample entry,
+        # 5: CMAF frag dur, 6: init constraints, 7: frag_type, 8: resolution, 9: framerate, 10: bitrate
+        reps += [{"resolution": row[8], "framerate": float(row[9])*fps_base, "bitrate": row[10], "input": row[1]}]
+        codec="h264"
+        cmaf_profile="avchdhf"
+        reps_command += "id:{0},type:video,codec:{1},vse:{2},cmaf:{3},fps:{4},res:{5},bitrate:{6},input:{7},sei:{8},vui_timing:{9}"\
+            .format(row[0], codec, row[4], cmaf_profile, float(row[9])*fps_base, row[8], row[10], row[1], row[2].capitalize(), row[3].capitalize())
 
-            reps_command += "\|"
+        # SS-X1
+        if row[0] in switching_set_X1_IDs:
+            if row[0] == switching_set_X1_IDs[0]:
+                switching_set_X1_command += "\|"
+            switching_set_X1_command += reps_command
 
-        #add audio
-        reps_command += "id:{0},type:audio,codec:aac,bitrate:{1},input:{2}"\
-            .format(len(resolutions[0])+1, resolutions[0][i][1], resolutions[0][i][3])
+        # Add audio
+        audio_command = "id:{0},type:audio,codec:aac,bitrate:{1},input:{2}"\
+            .format("a", "128k", row[1])
+        reps_command += "\|"
+        reps_command += audio_command
 
+        # SS-X1: add the first available audio
+        if line_count == 2:
+            switching_set_X1_command += "\|"
+            switching_set_X1_command += audio_command
+
+        #TODO: update the Web exposed information
         database[key] = {
             'representations': reps,
             'segmentDuration': row[5],
@@ -64,15 +71,19 @@ with open('params.csv') as csv_file:
             'hasSEI': row[1].lower() == 'true',
             'hasVUITiming': row[2].lower() == 'true',
             'visualSampleEntry': row[3],
-            'mpdPath': 'avc_sets/{0}/stream.mpd'.format(row[0])
+            'mpdPath': 'avc/15_30_60/t{0}/stream.mpd'.format(row[0])
         }
 
-        command = "./encode_dash.py --path=/usr/bin/ffmpeg --out=stream.mpd --outdir=output/{0} --dash=sd:{1},ft:{2} {3}".format(row[0], row[5], row[7], reps_command)
+        command = "./encode_dash.py --path=/opt/bin/gpac --out=stream.mpd --outdir=output/t{0} --dash=sd:{1},ft:{2} {3}".format(row[0], row[5], row[7], reps_command)
         print("Executing " + command)
         result = subprocess.run(command, shell=True)
 
-# Write the database to a file
-with open(filepath, 'w') as outfile:
+    print("Switching Set " + content.format("X", 1))
+    command = "./encode_dash.py --path=/opt/bin/gpac --out=stream.mpd --outdir=output/ss1 --dash=sd:2,ft:duration {0}".format(switching_set_X1_command)
+    result = subprocess.run(command, shell=True)
+
+
+with open(wwwfilepath, 'w') as outfile:
     json.dump(database, outfile)
 
 with pysftp.Connection(host=host, username=username, private_key=os.path.expanduser(os.environ['AKAMAI_PRIVATE_KEY']), cnopts=cnopts) as sftp:
@@ -80,7 +91,7 @@ with pysftp.Connection(host=host, username=username, private_key=os.path.expandu
 
     # Switch to a remote directory and put the data base
     sftp.cwd(outputFolder)
-    sftp.put(filepath, outputFolder + filepath)
+    sftp.put(wwwfilepath, outputFolder + wwwfilepath)
 
     # Create the directory structure if it does not exist
     for root, dirs, files in os.walk('./output', topdown=True):
