305895cc5c97db00740fdeb58bc9bf169c34aa37
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2023-04-06 11:51:25 -0400

    update SS1 script

--- ss1/switching_set_X1.sh
+++ ss1/switching_set_X1.sh
@@ -3,16 +3,17 @@ set -eux
 
 INPUT_DIR="$PWD/output/"
 OUTPUT_DIR="$PWD/output_ss1/"
-BATCH="2022-10-17"
+BATCH="2023-04-06"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
 
 FRAMERATES=( '12.5_25_50' '15_30_60' '14.985_29.97_59.94' )
 for FR in "${FRAMERATES[@]}" ; do
     mkdir -p cfhd_sets/$FR/ss1/$BATCH
     pushd cfhd_sets/$FR/ss1/$BATCH
-    cp $SCRIPT_DIR/avc_$FR_ss1_stream.mpd avc_$FR_ss1_stream.mpd
+    cp $SCRIPT_DIR/avc_"$FR"_ss1_stream.mpd avc_"$FR"_ss1_stream.mpd
 
     mkdir -p cfhd_sets/$FR/
     pushd cfhd_sets/$FR/
