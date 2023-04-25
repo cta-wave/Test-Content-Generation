32c4eccddb4ae9d78b1f2d72578a63ad21c0e8b2
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2023-04-25 15:37:14 -0400

    update ss1 script according to https://github.com/cta-wave/Test-Content-Generation/issues/60#issuecomment-1522000545

--- ss1/switching_set_X1.sh
+++ ss1/switching_set_X1.sh
@@ -2,7 +2,7 @@
 set -eux
 
 INPUT_DIR="$PWD/output/"
-OUTPUT_DIR="$PWD/output_ss1/"
+OUTPUT_DIR="$PWD/output_ss1/" # make it different of $INPUT_DIR to avoid collisions and deletions of input files
 BATCH="2023-04-06"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -11,30 +11,27 @@ cd $OUTPUT_DIR
 
 FRAMERATES=( '12.5_25_50' '15_30_60' '14.985_29.97_59.94' )
 for FR in "${FRAMERATES[@]}" ; do
-    mkdir -p cfhd_sets/$FR/ss1/$BATCH
-    pushd cfhd_sets/$FR/ss1/$BATCH
-    cp $SCRIPT_DIR/avc_"$FR"_ss1_stream.mpd avc_"$FR"_ss1_stream.mpd
+    SS1_OUTPUT_RELDIR=switching_sets/$FR/ss1/$BATCH
+    mkdir -p $SS1_OUTPUT_RELDIR
 
-    mkdir -p cfhd_sets/$FR/
-    pushd cfhd_sets/$FR/
+    cp $SCRIPT_DIR/avc_"$FR"_ss1_stream.mpd $SS1_OUTPUT_RELDIR/stream.mpd
+
     REPS=("t19" "t23" "t24" "t25" "t28" "t32" "t34")
     for SINGLE in "${REPS[@]}" ; do
-        mkdir $SINGLE
-        cp -r $INPUT_DIR/cfhd_sets/$FR/$SINGLE/$BATCH/1/* $SINGLE
+        SINGLE_DIR=cfhd_sets/$FR/$SINGLE/$BATCH/1/
+        mkdir -p $SINGLE_DIR
+        cp -r $INPUT_DIR/$SINGLE_DIR/* $SINGLE_DIR
     done
-    popd
 
-    mkdir -p chdf_sets/$FR/
-    pushd chdf_sets/$FR/
-    cp -r $INPUT_DIR/chdf_sets/$FR/t20 .
-    popd
+    mkdir -p chdf_sets/$FR/t20/$BATCH/1/
+    cp -r $INPUT_DIR/chdf_sets/$FR/t20/$BATCH/1/* chdf_sets/$FR/t20/$BATCH/1/
 
     # delete old archives
-    find . -name '*.zip' | xargs rm
+    #find . -name '*.zip' | xargs rm
 
     # create archive
-    zip -r ss1.zip .
+    zip -r $SS1_OUTPUT_RELDIR/ss1.zip cfhd_sets chdf_sets
 
-    popd
+    # delete duplicated files
+    rm -rf cfhd_sets chdf_sets
 done
-
