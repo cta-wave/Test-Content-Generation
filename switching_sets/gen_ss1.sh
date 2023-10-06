bf048ade299f9932f24136af74c0baf321888126
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2023-10-06 10:50:58 -0400

    update ss1 to 2023-10-05 - see https://github.com/cta-wave/Test-Content/issues/53

--- switching_sets/gen_ss1.sh
+++ switching_sets/gen_ss1.sh
@@ -3,28 +3,35 @@ set -eux
 
 INPUT_DIR="$PWD/output/"
 OUTPUT_DIR="$PWD/output_ss1/" # make it different of $INPUT_DIR to avoid collisions and deletions of input files
-BATCH="2023-09-01"
+BATCH_IN="2023-09-01" # input batch
+BATCH_OUT="2023-10-05" # Romain: $BATCH_IN # change when you need to regenerate the output in a different batch folder
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# safety check
+if ! grep $BATCH_IN $SCRIPT_DIR/avc_*.mpd >/dev/null 2>/dev/null; then
+    echo "ERROR: you must replace the batch dates in the MPD files with \"$BATCH_IN\". Make sure these files exist first."
+    exit 1
+fi
 
 mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
 
 FRAMERATES=( '12.5_25_50' '15_30_60' '14.985_29.97_59.94' )
 for FR in "${FRAMERATES[@]}" ; do
-    SS1_OUTPUT_RELDIR=switching_sets/$FR/ss1/$BATCH
+    SS1_OUTPUT_RELDIR=switching_sets/$FR/ss1/$BATCH_OUT
     mkdir -p $SS1_OUTPUT_RELDIR
 
     cp $SCRIPT_DIR/avc_"$FR"_ss1_stream.mpd $SS1_OUTPUT_RELDIR/stream.mpd
 
     REPS=("t19" "t23" "t24" "t25" "t28" "t32" "t34")
     for SINGLE in "${REPS[@]}" ; do
-        SINGLE_DIR=cfhd_sets/$FR/$SINGLE/$BATCH/1/
+        SINGLE_DIR=cfhd_sets/$FR/$SINGLE/$BATCH_IN/1/
         mkdir -p $SINGLE_DIR
         cp -r $INPUT_DIR/$SINGLE_DIR/* $SINGLE_DIR
     done
 
-    mkdir -p chdf_sets/$FR/t20/$BATCH/1/
-    cp -r $INPUT_DIR/chdf_sets/$FR/t20/$BATCH/1/* chdf_sets/$FR/t20/$BATCH/1/
+    mkdir -p chdf_sets/$FR/t20/$BATCH_IN/1/
+    cp -r $INPUT_DIR/chdf_sets/$FR/t20/$BATCH_IN/1/* chdf_sets/$FR/t20/$BATCH_IN/1/
 
     # delete old archives
     #find . -name '*.zip' | xargs rm
--- switching_sets/gen_ss2.sh
+++ switching_sets/gen_ss2.sh
@@ -3,22 +3,29 @@ set -eux
 
 INPUT_DIR="$PWD/output/"
 OUTPUT_DIR="$PWD/output_ss2/" # make it different of $INPUT_DIR to avoid collisions and deletions of input files
-BATCH="2023-09-01"
+BATCH_IN="2023-09-01" # input batch
+BATCH_OUT="2023-10-05" # Romain: $BATCH_IN # change when you need to regenerate the output in a different batch folder
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# safety check
+if ! grep $BATCH_IN $SCRIPT_DIR/hevc_*.mpd >/dev/null 2>/dev/null; then
+    echo "ERROR: you must replace the batch dates in the MPD files with \"$BATCH_IN\". Make sure these files exist first."
+    exit 1
+fi
 
 mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
 
 FRAMERATES=( '12.5_25_50' '15_30_60' '14.985_29.97_59.94' )
 for FR in "${FRAMERATES[@]}" ; do
-    SS2_OUTPUT_RELDIR=switching_sets/$FR/ss2/$BATCH
+    SS2_OUTPUT_RELDIR=switching_sets/$FR/ss2/$BATCH_OUT
     mkdir -p $SS2_OUTPUT_RELDIR
 
-    cp $SCRIPT_DIR/avc_"$FR"_ss2_stream.mpd $SS2_OUTPUT_RELDIR/stream.mpd
+    cp $SCRIPT_DIR/hevc_"$FR"_ss2_stream.mpd $SS2_OUTPUT_RELDIR/stream.mpd
 
     REPS=("t10" "t11" "t12")
     for SINGLE in "${REPS[@]}" ; do
-        SINGLE_DIR=chh1_sets/$FR/$SINGLE/$BATCH/1/
+        SINGLE_DIR=chh1_sets/$FR/$SINGLE/$BATCH_IN/1/
         mkdir -p $SINGLE_DIR
         cp -r $INPUT_DIR/$SINGLE_DIR/* $SINGLE_DIR
     done
