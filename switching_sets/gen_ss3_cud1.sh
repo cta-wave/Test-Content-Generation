ba208ce7bac6c9a60d0aa3503beb65100deb61e1
Nils <nils.dvl+git@gmail.com>
2025-01-21 12:52:34 +0100

    update switching set generation scripts to process hevc content

---
+++ switching_sets/gen_ss3_cud1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eux
+
+SSi="ss3"
+REPS=("t30" "t31" "t32")
+PROFILE="cud1"
+BATCH_IN="2025-01-15" # input batch
+BATCH_OUT=$BATCH_IN # change when you need to regenerate the output in a different batch folder
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+INPUT_DIR="$SCRIPT_DIR/../output"
+OUTPUT_DIR="$SCRIPT_DIR/../output_ss_$PROFILE" # make it different of $INPUT_DIR to avoid collisions and deletions of input files
+
+# safety check
+if ! grep $BATCH_IN $SCRIPT_DIR/"$PROFILE"_*.mpd >/dev/null 2>/dev/null; then
+    echo "ERROR: you must replace the batch dates in the MPD files with \"$BATCH_IN\". Make sure these files exist first."
+    exit 1
+fi
+
+mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
+
+FRAMERATES=( '12.5_25_50' '15_30_60' '14.985_29.97_59.94' )
+
+for FR in "${FRAMERATES[@]}" ; do
+
+    SS_OUTPUT_RELDIR=switching_sets/$FR/"$SSi"_"$PROFILE"/$BATCH_OUT
+    mkdir -p $SS_OUTPUT_RELDIR
+    cp $SCRIPT_DIR/"$PROFILE"_"$FR"_"$SSi"_stream.mpd $SS_OUTPUT_RELDIR/stream.mpd
+
+    for SINGLE in "${REPS[@]}" ; do
+        SINGLE_DIR="$PROFILE"_sets/$FR/$SINGLE/$BATCH_IN/1
+        mkdir -p $SINGLE_DIR
+        cp -r $INPUT_DIR/$SINGLE_DIR/* $SINGLE_DIR
+    done
+
+    # delete old archives
+    #find . -name '*.zip' | xargs rm
+
+    # create archive
+    zip -r $SS_OUTPUT_RELDIR/"$SSi"_$PROFILE.zip "$PROFILE"_sets $SS_OUTPUT_RELDIR
+
+    # delete duplicated files
+    rm -rf $OUTPUT_DIR/"$PROFILE"_sets
+done
